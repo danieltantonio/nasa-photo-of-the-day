16dbc8c08d16b15cf2497038151c99864ab97fd7
Daniel Antonio <danieltomantonio@gamil.com>
2020-08-13 16:02:34 -0700

    Added date functionality to ContentSection component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,48 @@ import "./App.css";
 import Navbar from './components/Navbar/Navbar'
 import ContentSection from './components/ContentSection/ContentSection';
 
+const initTimeMachine = {
+  year: 0,
+  month: 0,
+  day: 0
+}
+
 function App() {
   const [imgData, setImgData] = useState('');
+  const [timeMachine, setTimeMachine] = useState(initTimeMachine);
 
-   useEffect(() => {
+  const onDateChange = (name, value) => {
+    setTimeMachine({
+      ...timeMachine,
+      [name]: value
+    })
+  };
+
+  const onSubmitChange = data => {
+    let month = data.month;
+    let day = data.day;
+
+    if(month.length === 1) {
+      month = 0 + month
+    }
+
+    if(day.length === 1) {
+      day = 0 + day
+    }
+
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&date=${data.year}-${month}-${day}`)
+      .then(res => {
+        console.log(res.data);
+        setImgData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  useEffect(() => {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`)
     .then(res => {
-      console.log(res.data)
       setImgData(res.data)
     })
     .catch(err => {
@@ -23,7 +58,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <ContentSection data={imgData}  />
+      <ContentSection data={imgData} onDateChange={onDateChange} onSubmitChange={onSubmitChange} timeMachine={timeMachine} />
     </div>
   );
 }
--- src/components/ContentSection/ContentSection.js
+++ src/components/ContentSection/ContentSection.js
@@ -14,10 +14,32 @@ const ContentSectionComp = styled.div`
 
 
 function ContentSection(props) {
-    const {data} = props;
+    const {data, timeMachine, onDateChange, onSubmitChange} = props;
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      onSubmitChange(timeMachine)
+    };
+
+    const onInputChange = e => {
+      const { name, value } = e.target;
+      onDateChange(name, value);
+    }
 
     return(
       <ContentSectionComp className="Content-Section">
+          <div className='time-machine'>
+            <p>Choose another picture by date:</p>
+            <form onSubmit={onSubmit}>
+              <label>Month: </label>
+              <input type='number' name='month' min={1} max={12} value={timeMachine.month} onChange={onInputChange}/>
+              <label> Day: </label>
+              <input type='number' name='day' min={1} max={31} value={timeMachine.day} onChange={onInputChange}/>
+              <label> Year: </label>
+              <input type='number' name='year' min={1996} max={2020} value={timeMachine.year} onChange={onInputChange}/>
+              <button>Submit</button>
+            </form>
+          </div>
           <div className="Image-Section">
             <img src={data.url} />
           </div>
